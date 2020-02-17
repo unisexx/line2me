2d33e0ea11fa52f733533ee3dd3e2a8b26d1d617
unisexx <unisexx@gmail.com>
2020-02-17 21:37:31 +0700

    updated

---
+++ resources/views/include/front/_google_custom_search.blade.php
@@ -0,0 +1,11 @@
+<script async src="https://cse.google.com/cse.js?cx=008951094756729902779:xugaf7h99xf"></script>
+<div class="gcse-search"></div>
+
+<style>
+body td.gsc-input{
+    padding-left: 50px !important;
+}
+body .gsc-control-cse{
+    margin-bottom: -25px !important;
+}
+</style>
