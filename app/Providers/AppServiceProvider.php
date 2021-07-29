e14326378cf8b8005c4fb560674e0ea877e05a8e
จอห์น ชาวไร่ <unisexx@gmail.com>
2021-07-29 15:54:03 +0700

    updated

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,8 +2,12 @@
 
 namespace App\Providers;
 
-use Illuminate\Support\ServiceProvider;
+use App\Models\Promote;
+use App\Models\Series;
+use Cache;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\View;
+use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -25,5 +29,30 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Schema::defaultStringLength(191);
+
+        View::share('sticker_promote',
+            Cache::remember('sticker_promote', config('calculations.cache_time'), function () {
+                return Promote::where('product_type', '=', 'sticker')->where('end_date', '>=', Carbon::now()->toDateString())->with('sticker')->orderBy('id', 'desc')->get();
+            })
+        );
+
+        View::share('theme_promote',
+            Cache::remember('theme_promote', config('calculations.cache_time'), function () {
+                return Promote::where('product_type', '=', 'theme')->where('end_date', '>=', Carbon::now()->toDateString())->with('theme')->orderBy('id', 'desc')->get();
+            })
+        );
+
+        View::share('emoji_promote',
+            Cache::remember('emoji_promote', config('calculations.cache_time'), function () {
+                return Promote::where('product_type', '=', 'emoji')->where('end_date', '>=', Carbon::now()->toDateString())->with('emoji')->orderBy('id', 'desc')->get();
+            })
+        );
+
+        View::share('serie_promote',
+            Cache::remember('editor_pick', now()->addMinutes(3), function () {
+                return Series::where('status', 1)->take(3)->inRandomOrder()->get();
+            })
+        );
+
     }
 }
--- resources/views/include/front/_promote_section.blade.php
+++ resources/views/include/front/_promote_section.blade.php
@@ -1,56 +1,3 @@
-@php
-// สติ๊กเกอร์ไลน์โปรโมท
-// $sticker_promote = DB::table('promotes')
-// 	->join('stickers', 'promotes.product_code', '=', 'stickers.sticker_code')
-// 	->select('stickers.*')
-// 	->where('promotes.product_type', '=', 'sticker')
-// 	->where('promotes.end_date', '>=', Carbon::now()->toDateString())
-// 	->inRandomOrder()
-// 	->take(30)
-// 	->get();
-
-// ธีมไลน์โปรโมท
-// $theme_promote = DB::table('promotes')
-// 	->join('themes', 'promotes.product_code', '=', 'themes.id')
-// 	->select('themes.*')
-// 	->where('promotes.product_type', '=', 'theme')
-// 	->where('promotes.end_date', '>=', Carbon::now()->toDateString())
-// 	->inRandomOrder()
-// 	->take(30)
-// 	->get();
-
-
-// อิโมจิไลน์โปรโมท
-// $emoji_promote = DB::table('promotes')
-// 	->join('emojis', 'promotes.product_code', '=', 'emojis.emoji_code')
-// 	->select('emojis.*')
-// 	->where('promotes.product_type', '=', 'emoji')
-// 	->where('promotes.end_date', '>=', Carbon::now()->toDateString())
-// 	->inRandomOrder()
-// 	->take(30)
-// 	->get();
-
-// editorpick
-// $series = App\Models\Series::where('status', 1)->take(3)->inRandomOrder()->get();
-
-
-$sticker_promote = Cache::remember('sticker_promote', config('calculations.cache_time'), function() {
-	return App\Models\Promote::where('product_type', '=', 'sticker')->where('end_date', '>=', Carbon::now()->toDateString())->with('sticker')->orderBy('id', 'desc')->get();
-});
-
-$theme_promote = Cache::remember('theme_promote', config('calculations.cache_time'), function() {
-	return App\Models\Promote::where('product_type', '=', 'theme')->where('end_date', '>=', Carbon::now()->toDateString())->with('theme')->orderBy('id', 'desc')->get();
-});
-
-$emoji_promote = Cache::remember('emoji_promote', config('calculations.cache_time'), function() {
-	return App\Models\Promote::where('product_type', '=', 'emoji')->where('end_date', '>=', Carbon::now()->toDateString())->with('emoji')->orderBy('id', 'desc')->get();
-});
-
-$series = Cache::remember('editor_pick', now()->addMinutes(3), function() {
-	return App\Models\Series::where('status', 1)->take(3)->inRandomOrder()->get();
-});
-@endphp
-
 @if(count($sticker_promote) != 0)
 <div class="fh5co-narrow-content">
 	<div class="d-flex justify-content-between align-items-baseline animate-box" data-animate-effect="fadeInLeft">
@@ -99,7 +46,7 @@ $series = Cache::remember('editor_pick', now()->addMinutes(3), function() {
 		<p class="text-right read-more-text"><a href="{{ url('series') }}">ดูทั้งหมด ></a></p>
 	</div>
 	<div class="row mb-5">
-		@foreach($series as $row)
+		@foreach($serie_promote as $row)
 		<div class="col pl-2 pr-2">
 			<a href="{{ url('series/'. $row->id) }}">
 				<img class="img-fluid" src="{{ @$row->image ?? 'https://dummyimage.com/526x250/fff' }}" title="{{ @$row->title }}" alt="{{ @$row->sub_title }}">
