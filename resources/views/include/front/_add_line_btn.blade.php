18bfdd0c046267062e0ae812941ca83aac2c406e
จอห์น ชาวไร่ <unisexx@gmail.com>
2021-07-30 22:56:59 +0700

    updated

--- resources/views/include/front/_add_line_btn.blade.php
+++ resources/views/include/front/_add_line_btn.blade.php
@@ -5,7 +5,7 @@
                 <a class="btn btn-success btn-block btn-sm" href="https://line.me/ti/p/~ratasak1234" target="_blank">สั่งซื้อคลิก (แอดไลน์ ratasak1234)</a>
             </div>
             <div class="col-sm-6">
-                <button class="btn btn-block btn-danger btn-sm">แอดไลน์ร้านไม่ได้ทักแชทด้านล่างครับ</button>
+                <a class="btn btn-block btn-danger btn-sm" href="{{ url('page/view/23') }}">แอดไลน์ร้านไม่ได้คลิกที่นี่จ้า</a>
             </div>
         </div>
     <hr>
